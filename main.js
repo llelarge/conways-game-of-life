ecddf3230c59364ed3b8db40b24dc48d596f3fb1
Léa Le Large <lea.le-large@insa-lyon>
2022-05-05 16:15:47 -0500

    trying to scroll and zoom

--- main.js
+++ main.js
@@ -452,6 +452,21 @@ function reset(){
   location.reload();
 }
 
+const zoomElement = document.querySelector(".zoom");
+let zoom = 1;
+const ZOOM_SPEED = 0.1;
+
+document.addEventListener("wheel", function(e) {
+
+    if(e.deltaY > 0){
+        zoomElement.style.transform = `scale(${zoom += ZOOM_SPEED})`;
+    }else{
+        zoomElement.style.transform = `scale(${zoom -= ZOOM_SPEED})`;  }
+
+});
+
+
+
 window.onload=()=>{
     createGrid();
     createArrays();
